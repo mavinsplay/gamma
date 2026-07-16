3a99d06ca7e485eb101bd4b761485f60e973e94d
o2o <Mavinsplay@yandex.ru>
2026-07-16 12:03:39 +0300

    flake8 fix

--- bot.py
+++ bot.py
@@ -86,13 +86,21 @@ async def command_start_handler(message: types.Message) -> None:
             [
                 InlineKeyboardButton(
                     text="📜 Политика конфиденциальности",
-                    url="https://telegra.ph/Politika-konfidencialnosti-06-21-31",
+                    url=(
+                        "https://telegra.ph/"
+                        "Politika-konfidencialnosti-"
+                        "06-21-31"
+                    ),
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text="📜 Пользовательское соглашение",
-                    url="https://telegra.ph/Polzovatelskoe-soglashenie-04-01-19",
+                    url=(
+                        "https://telegra.ph/"
+                        "Polzovatelskoe-soglashenie-"
+                        "04-01-19"
+                    ),
                 ),
             ],
         ],
